3d3d461deac626809c6a74ca7e3153a0e2f4cb32
root <root@centos.vbox>
2014-07-25 05:05:51 -0400

    added using root

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ echo 'the quick brown fox jumps over the lazy dog.';
 echo 'hello this is a test from archie mercader<br>';
 echo 'lets push this again<br>';
 echo 'this is final'. date("Y").'<br>';
-echo 'hello world';
+echo 'hello world<br>';
+echo 'this is modified by root<br>';
 
 ?>
