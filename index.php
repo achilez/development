fd5439d30cc3123d0686fc11105ccf5339b52fed
Achiles Archie Mercader <achilez@gmail.com>
2014-07-25 05:04:02 -0400

    last push

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo 'this is the index.php file for proj01.local<br>';
 echo 'the quick brown fox jumps over the lazy dog.';
 echo 'hello this is a test from archie mercader<br>';
 echo 'lets push this again<br>';
-echo 'this is final'. date();
+echo 'this is final'. date("Y").'<br>';
 echo 'hello world';
 
 ?>
