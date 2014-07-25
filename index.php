345e2bf6777181d4e12631d4b466a6b947dc2aa9
Achiles Archie Mercader <achilez@gmail.com>
2014-07-25 00:33:28 -0400

    last update

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-echo 'this is the index.php file<br>';
+echo 'this is the index.php file for proj01.local<br>';
 echo 'the quick brown fox jumps over the lazy dog.';
 echo 'hello this is a test from archie mercader<br>';
 echo 'lets push this again<br>';
+echo 'this is final'. date();
 
 ?>
