92a327a00ee531f5134be4c04c7ad1c05665a830
Archie Mercader <achilez@gmail.com>
2015-07-22 13:09:57 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo 'this is the index.php file for test1.dev<br>';
+echo 'this is the index.php file for codeigniter.dev<br>';
 echo 'the quick brown fox jumps over the lazy dog.';
 echo 'hello this is a test from archie mercader<br>';
 echo 'lets push this again<br>';
